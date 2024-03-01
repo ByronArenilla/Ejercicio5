c403b437ec2eb119fca12c53df2ba4bd19136683
Byron Arenilla <barenilla@unal.edu.co>
2024-03-01 17:16:57 -0500

    Primer Commit

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,18 @@
+
+package ejercicio5;
+
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        double radio,area, longitud;
+        radio = 15;
+        area = Math.PI * Math.pow(radio, 2);
+        longitud = 2 * Math.PI * radio;
+        
+        System.out.println("El radio es: "+radio);
+        System.out.println("El area es: " +area);
+        System.out.println("La longitud es: "+ longitud);
+        
+    }
+}
